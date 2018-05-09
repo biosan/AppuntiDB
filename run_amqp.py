051f7e65ad8742317b7d4de6c628adc76af6ee69
Alessandro Biondi <alessandro@biondi.me>
2018-05-09 10:57:58 +0200

    Fixed page request query name

--- run_amqp.py
+++ run_amqp.py
@@ -4,5 +4,5 @@ import os
 from db_api.amqp import AMQP
 
 if __name__ == '__main__':
-    amqp = AMQP(os.environ.get('AMQP_BROKER_URL'), 'users_queue', 'notes_queue', 'SEARCH_QUERY_REQUEST', 'SEARCH_QUERY_RESPONSE')
+    amqp = AMQP(os.environ.get('AMQP_BROKER_URL'), 'users_queue', 'PAGE_QUERY', 'SEARCH_QUERY_REQUEST', 'SEARCH_QUERY_RESPONSE')
     amqp.start()
